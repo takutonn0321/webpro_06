b046060b2944ef577014aa4374ded527d6073c34
Takumi Uehara <u.takumi0321@icloud.com>
2024-10-31 16:11:54 +0900

    error

--- app5.js
+++ app5.js
@@ -29,8 +29,8 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
+  let win = Number( req.query.win ) || 0;
+  let total = Number( req.query.total ) || 0;
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
